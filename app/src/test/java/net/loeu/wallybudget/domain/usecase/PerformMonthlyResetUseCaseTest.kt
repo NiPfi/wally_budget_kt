3a4a6f77f95a2ea0fb979c28f321d43070630827
Nicola Pfister <1059042+NiPfi@users.noreply.github.com>
2026-03-10 16:03:12 +0100

    Remove unused import from reset use case test

--- app/src/test/java/net/loeu/wallybudget/domain/usecase/PerformMonthlyResetUseCaseTest.kt
+++ app/src/test/java/net/loeu/wallybudget/domain/usecase/PerformMonthlyResetUseCaseTest.kt
@@ -5,7 +5,6 @@ import net.loeu.wallybudget.domain.model.UserSettings
 import net.loeu.wallybudget.domain.service.BudgetCalculationService
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertNull
 import org.junit.Test
 import java.time.LocalDate
 import java.time.ZoneId
